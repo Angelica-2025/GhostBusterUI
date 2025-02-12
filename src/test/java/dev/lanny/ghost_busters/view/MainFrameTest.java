4988b2135c843beb31e332c9a283324782b622b4
LannyRivero <lannyrivero288@gmail.com>
2025-02-12 20:05:40 +0100

    Add test to verify MainFrame creation and visibility

--- src/test/java/dev/lanny/ghost_busters/view/MainFrameTest.java
+++ src/test/java/dev/lanny/ghost_busters/view/MainFrameTest.java
@@ -1,23 +1,25 @@
 package dev.lanny.ghost_busters.view;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import java.util.ArrayList;
 
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import dev.lanny.ghost_busters.controller.HunterController;
 import dev.lanny.ghost_busters.model.HunterModel;
 
 public class MainFrameTest {
-   private FrameFixture window;
+    private FrameFixture window;
 
     @BeforeEach
     void setUp() throws InterruptedException {
         MainFrame mainFrame = GuiActionRunner.execute(() -> new MainFrame(
-            new HunterController(new HunterModel("Egon", new ArrayList<>()))
-        ));
+                new HunterController(new HunterModel("Egon", new ArrayList<>()))));
         window = new FrameFixture(mainFrame);
         window.show();
         Thread.sleep(500);
@@ -27,4 +29,12 @@ public class MainFrameTest {
     void tearDown() {
         window.cleanUp();
     }
+
+    @Test
+    void shouldCreateMainFrame() {
+        assertNotNull(window);
+        window.requireVisible();
+        window.requireTitle("👻 GhostBusters Asturias - Base de Operaciones");
+    }
+
 }
